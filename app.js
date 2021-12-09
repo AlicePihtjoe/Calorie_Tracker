598c19f20b33b2d46f265789eacde65de1e2fbf0
AlicePihtjoe <alice.pihtjoe@khk.ee>
2021-12-09 10:38:15 +0200

    Saw differences in console when testing

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // module pattern basic structure
 // standard module pattern
 // which will be UI Controller
-
+/*
 const UIController = (function(){
     // declare private variables and functions
 let text = 'Hello World'
@@ -20,3 +20,36 @@ let text = 'Hello World'
 })();
 
 UIController.callChangeText()
+
+ */
+
+//revealing module pattern
+//it will be Item controller
+
+const ItemController = (function() {
+    let data = []
+
+    function add(item){
+        data.push(item)
+        console.log('item is added')
+    }
+
+    function get(id){
+        return data.find(item=> {
+            return item.id === id
+        })
+    }
+ // siit tuleb alles avalik osa
+
+    return {
+        add: add,
+        get: get
+    }
+
+})();
+
+//testimiseks lisan:
+
+ItemController.add({id: 1, name: 'Alice'})
+//konsoolis sisu kontrollimiseks
+console.log(ItemController.get(1))
